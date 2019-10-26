bded4176d7cd3a27fe1b22d72a95ca88d6ba72d3
Shrikant Vishwakarma <zeeroiq@gmail.com>
2019-10-26 19:02:43 +0530

    Custom authentication using inMemoryAuthentication and NoOpPasswordEncoding

--- src/main/java/com/shri/springsecurity/spring/SpringSecurityApplication.java
+++ src/main/java/com/shri/springsecurity/spring/SpringSecurityApplication.java
@@ -2,13 +2,13 @@ package com.shri.springsecurity.spring;
 
 import com.shri.springsecurity.persistence.InMemoryUserRepository;
 import com.shri.springsecurity.persistence.UserRepository;
+import com.shri.springsecurity.spring.config.CustomSecurityConfig;
+import com.shri.springsecurity.web.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.convert.converter.Converter;
-
-import com.shri.springsecurity.web.model.User;
 
 @SpringBootApplication
 @ComponentScan("com.shri.springsecurity.web")
@@ -30,7 +30,9 @@ public class SpringSecurityApplication {
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(SpringSecurityApplication.class, args);
+        SpringApplication.run(new Class[] {
+                SpringSecurityApplication.class, CustomSecurityConfig.class
+        }, args);
     }
 
 }
--- src/main/java/com/shri/springsecurity/spring/config/CustomSecurityConfig.java
+++ src/main/java/com/shri/springsecurity/spring/config/CustomSecurityConfig.java
@@ -4,15 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class CustomSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
+        auth
+                .inMemoryAuthentication()
                 .withUser("user")
                 .password("password")
-                .roles("USER");
+                .roles("USER")
+                .and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
